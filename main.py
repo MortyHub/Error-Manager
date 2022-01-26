7ea90854b2e5d44f20c3cb98f56d3b3214886c9d
CaKa12 <83893877+MortyHub@users.noreply.github.com>
2022-01-26 02:51:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+Types = ["Error: ", "Warning: ", "Alert: "]
+
+
+class Error:
+    def __init__(self, name, text, typee):
+        self.errorName = name
+        self.errorText = text
+        self.errorType = typee
+
+    def printError(self):
+        if self.errorType == "Error":
+            colored(255, 51, 51, "Error: " + self.errorText)
+        
+        if self.errorType == "Warning":
+            colored(235, 94, 94, "Warning: " + self.errorText)
+        
+        if self.errorType == "Alert":
+            colored(255, 245, 84, "Alert: " + self.errorText)
+
+def colored(r, g, b, text):
+    print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text))
+
+Error1 = Error("Hi", "your mom is gay", "Warning")
+Error1.printError()
+
